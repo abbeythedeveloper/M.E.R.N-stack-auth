511a61466fc4bd7666d95d3cbd6222fb28e5a2ff
Abbeythedeveloper <ogundiranabbey057@gmail.com>
2023-10-08 14:16:20 -0700

    mm

--- server/Index.js
+++ server/Index.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 const { mongoose } = require('mongoose')
 const cookieParser = require('cookie-parser')
-const cookieSession = require('cookie-session')
 // const authRoute = require('./Routes/authRoutes')
 
 const app = express();
