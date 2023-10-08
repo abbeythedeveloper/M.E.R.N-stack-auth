d0b65a86a815bdde4590afeff7b4507c93e5e699
Abbeythedeveloper <ogundiranabbey057@gmail.com>
2023-10-08 13:00:53 -0700

    z

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container mx-auto pl-2 text-3xl flex items-center w-full justify-center pt-5'>Welcome to my app 🥳, i tried my best🥲. click the register button to continue</div>
+    <div className='container mx-auto pl-2 text-3xl flex items-center w-full justify-center pt-5'>Welcome to my app 🥳, i tried my best🥲. click the register button to continue.</div>
   )
 }
 
